9f0809b7f67fff4f07c4dd4b858ae0550169725a
lalitS <lgsangore2002@gmail.com>
2024-11-11 00:42:05 +0530

    Calculate Wages till a condition of total working hours or days is reached for a month

--- Employee_wage.py
+++ Employee_wage.py
@@ -3,6 +3,7 @@ wage_per_hour=20
 full_day_hour=8
 part_time_day_hour=4
 month_days=20
+max_hour_per_month=100
 def check_attendance():
     return random.choice([0,1,2])
 
@@ -12,22 +13,31 @@ def calculate_daily_wage():
 def part_time_employee_daily_wage():
     return part_time_day_hour*wage_per_hour
 
-def calculate_monthly_wage():
-    total_daily_wage = []
-    for _ in range(month_days):
+def calculate_monthly_wage_for_20days_Max100hrs():
+    total_hours = 0
+    total_days = 0
+    total_wage =0
+    while total_hours < max_hour_per_month and total_days < month_days:
         attendance = check_attendance()
         if attendance == 1:
-            total_daily_wage.append(calculate_daily_wage())
-            
+            total_hours += full_day_hour
+            total_wage += calculate_daily_wage()
         elif attendance == 2:
-            total_daily_wage.append(part_time_employee_daily_wage())
+            total_hours += part_time_day_hour
+            total_wage += part_time_employee_daily_wage()
         else :
-            total_daily_wage.append(0)
-    return sum(total_daily_wage), total_daily_wage
+            total_hours += 0
+            total_wage += 0
+        total_days +=1
+    return total_days, total_hours, total_wage
+     
         
 
 if __name__=='__main__':
     print("***Welcome to Employee Wage Computation Program***")
-    monthly_wage, day_wise_wage = calculate_monthly_wage()
-    print(f"Given Employee monthly wages is : {monthly_wage}")
-    print(f"Given Employee day wise monthly wages  : {day_wise_wage}")
+    total_days, total_hours, total_wages = calculate_monthly_wage_for_20days_Max100hrs()
+    print(f"Employee Total Present Days in Month : {total_days} ")
+    print(f"Employee Total Working Hours in Month : {total_hours} ")
+    print(f"Employee Total Wages for one Month  : {total_wages} ")
+
+    
