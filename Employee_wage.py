89907a7fee8d1cae28c531dbe9b2b6e8f02d6bb0
lalitS <lgsangore2002@gmail.com>
2024-11-11 00:11:46 +0530

    Calculating Wages for a Month

--- Employee_wage.py
+++ Employee_wage.py
@@ -2,6 +2,7 @@ import random
 wage_per_hour=20
 full_day_hour=8
 part_time_day_hour=4
+month_days=20
 def check_attendance():
     return random.choice([0,1,2])
 
@@ -11,14 +12,22 @@ def calculate_daily_wage():
 def part_time_employee_daily_wage():
     return part_time_day_hour*wage_per_hour
 
+def calculate_monthly_wage():
+    total_daily_wage = []
+    for _ in range(month_days):
+        attendance = check_attendance()
+        if attendance == 1:
+            total_daily_wage.append(calculate_daily_wage())
+            
+        elif attendance == 2:
+            total_daily_wage.append(part_time_employee_daily_wage())
+        else :
+            total_daily_wage.append(0)
+    return sum(total_daily_wage), total_daily_wage
+        
+
 if __name__=='__main__':
-    print("Welcome to Employee Wage Computation Program")
-    attendance=check_attendance()
-    match (attendance):
-        case 1:
-            print('Employee is present')
-            print(f'Employee daily wage: {calculate_daily_wage()}')
-        case 2:
-            print(f'Employee works part time and there daily wage is {part_time_employee_daily_wage()}')
-        case 0:
-            print('Employee is absent')
+    print("***Welcome to Employee Wage Computation Program***")
+    monthly_wage, day_wise_wage = calculate_monthly_wage()
+    print(f"Given Employee monthly wages is : {monthly_wage}")
+    print(f"Given Employee day wise monthly wages  : {day_wise_wage}")
