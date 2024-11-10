96f6f510d0cef6f1958318fecd0af289c5532f5e
lalitS <lgsangore2002@gmail.com>
2024-11-10 23:20:11 +0530

    Calculate Daily Employee Wage

--- Employee_wage.py
+++ Employee_wage.py
@@ -1,11 +1,17 @@
 import random 
+wage_per_hour=20
+full_day_hour=8
 def check_attendance():
     return random.choice([0,1])
+
+def calculate_daily_wage():
+    return wage_per_hour*full_day_hour
 
 if __name__=='__main__':
     print("Welcome to Employee Wage Computation Program")
     attendance=check_attendance()
     if attendance==1:
         print('Employee is present')
+        print(f'Employee daily wage: {calculate_daily_wage()}')
     elif attendance==0:
         print('Employee is absent')
